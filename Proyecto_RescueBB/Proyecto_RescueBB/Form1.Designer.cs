b76a552b7b851365e4d4f6fdadc7de008b07bbf0
Tarde <Tarde@ASATA408>
2021-09-07 19:30:39 +0200

    TO DO: Pantalla Principal

--- Proyecto_RescueBB/Proyecto_RescueBB/Form1.Designer.cs
+++ Proyecto_RescueBB/Proyecto_RescueBB/Form1.Designer.cs
@@ -29,7 +29,15 @@ namespace Proyecto_RescueBB
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(326, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -37,13 +45,17 @@ namespace Proyecto_RescueBB
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_RescueBB.Properties.Resources.Gray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
